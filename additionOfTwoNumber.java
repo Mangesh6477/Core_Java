cbf71c205e74b336d5c85dbfbfbef4de0b4cc43f
Mangesh6477 <mangeshdeore6477@gmail.com>
2026-08-30 01:00:03 +0530

    Added Addition Of Two Number

---
+++ additionOfTwoNumber.java
@@ -0,0 +1,11 @@
+public class additionOfTwoNumber {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+
+        System.out.println("Addition = " + sum);
+    }
+}
