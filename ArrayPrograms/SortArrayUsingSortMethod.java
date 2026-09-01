dc3d62243fd0e9c36d96b14061501722205083ae
Mangesh6477 <mangeshdeore6477@gmail.com>
2026-09-01 11:38:38 +0530

    sort array using sort method

--- ArrayPrograms/SortArrayUsingSortMethod.java
+++ ArrayPrograms/SortArrayUsingSortMethod.java
@@ -2,26 +2,26 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class SortArrayUsingSortMethod{
-  public static void main(String[] args) {
+public class SortArrayUsingSortMethod
+{
+  public static void main(String[] args)
+   {
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter the size of the array");
     int size=sc.nextInt();
     int []array=new int[size];
     System.out.println("enter the arrays elements");
-    for (int i = 0; i <array.length; i++) {
+    for (int i = 0; i <array.length; i++) 
+    {
       int elements=sc.nextInt();
       array[i]=elements;
         
     }
      Arrays.sort(array);
-    for(int arr:array)
-    {
-      System.out.print(" "+arr);
-    }
-
-   
-      
+     for(int arr:array)
+     {
+       System.out.print(" "+arr);
+     }   
   }
 
 }
