f859d6895dccb68b4ba674b3360756efb619d743
Mangesh6477 <mangeshdeore6477@gmail.com>
2026-09-09 21:15:16 +0530

    delete

--- Java8/Consumer/PrintTheElement.java
+++
@@ -1,8 +0,0 @@
-
-      
-void main() {
-        List<String> names = Arrays.asList("mangesh", "pawan", "gaurav");
-        Consumer<String> consumer = values -> System.err.println(values);
-        names.forEach(consumer);
-
-    }
